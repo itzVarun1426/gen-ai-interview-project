58bb3b3736838aaf741fe08d1a72b6740541aed1
varun <varunnagote1426@gmail.com>
2026-06-24 21:26:54 +0530

    changed .env

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-import 'dotenv/config.js'
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
 import app from './src/app.js';
 import connectDB from './src/db/db.js';
 // import {generateInterviewReport} from './src/services/ai.service.js';
